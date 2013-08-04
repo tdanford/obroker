ef4b599b6c4fa3ce8f3db8b69abe8d1869fbf207
Timothy Danford <tdanford@gmail.com>
2013-08-04 10:35:11 -0400

    Adding json schemas for request/response types.

---
+++ src/main/resources/schemas/Link.js
@@ -0,0 +1,10 @@
+ { 
+   "name": "Link", 
+   "type": "object",
+   "properties": {
+     "href" : { 
+       "type" : "string",
+       "description" : "URL for the associatied link" 
+     }
+   }
+ }
---
+++ src/main/resources/schemas/Request.js
@@ -0,0 +1,65 @@
+{ 
+   name : "Request",
+   type : "object",
+   description : "A Request is an entry signifying a request for a new term, lodged with the Ontology Broker, and intended to be satisified by an Ontology Maintainer through interactions with the same Broker.",
+   
+   properties : { 
+
+    provisional_term : { 
+        type : "string",
+    },
+    
+    ontology_term : { 
+        type : "string",
+        optional : true,
+    },
+       
+    search_text : { 
+        type : "string", 
+    },
+    
+    context : { 
+        type : "string", 
+    },
+    
+    provenance : { 
+        type : "string",
+        optional : true,
+    },
+    
+    comment : { 
+        type : "string",
+        optional : true,
+    },
+    
+    status : { 
+        type : "integer",
+    },
+
+    creator : { 
+        type : "Link",
+        description : "A reference to the creator of this request.",
+    },
+    
+    modified_by : { 
+        type : "Link",
+		optional: true,
+    },
+    
+    date_submitted : { 
+        type : "string",
+    },
+     
+     ontology : { 
+        type : "Link",
+     },
+     
+     metadata : {
+       type : "array",
+       items : {
+         type : "Metadata"
+       }
+     }
+     
+   }
+}
